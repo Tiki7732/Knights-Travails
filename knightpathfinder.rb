47e0b7b26cd7cc16ff61147263fabad7cc422983
Laurent Saidah <lsaidah@gmail.com>
2020-05-29 12:09:20 -0500

    adding methods to this class

--- knightpathfinder.rb
+++ knightpathfinder.rb
@@ -1,8 +1,30 @@
 require_relative 'polytree'
 class KnightPathFinder
 
+    def self.valid_moves(pos)
+        x, y = pos
+        possible_moves = []
+        
+    end
+
     def initialize(pos)
         @grid = Array.new(8){Array.new(8)}
-        self.root_node = PolytreeNode.new(pos)
+        root_node = PolyTreeNode.new(pos)
+        build_move_tree
+        @considered_positions = [pos]
+    end
+
+    def build_move_tree
+
+    end
+
+    def new_move_positions(pos)
+
+    end
+
+    def find_path
+
     end
 end
+
+knight = KnightPathFinder.new([0,1])
