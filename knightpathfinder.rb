455c451648a93616a2b36465315596bf28a2e495
Laurent Saidah <lsaidah@gmail.com>
2020-05-29 11:45:32 -0500

    adding initialize method

--- knightpathfinder.rb
+++ knightpathfinder.rb
@@ -1,4 +1,8 @@
-require 'polytree'
+require_relative 'polytree'
 class KnightPathFinder
 
+    def initialize(pos)
+        @grid = Array.new(8){Array.new(8)}
+        self.root_node = PolytreeNode.new(pos)
+    end
 end
--- knightstravails.rb
+++
@@ -1,4 +0,0 @@
-
-class KnightsTravails
-
-end
